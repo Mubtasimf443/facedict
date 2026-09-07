208b23def4fbb37a1003e8c46d1a6020682f5d5a
Mubtasim <mubtasimf443@gmail.com>
2026-09-07 23:39:18 +0600

    dist folder updated of backend

--- backend/dist/config/env.js
+++ backend/dist/config/env.js
@@ -1,11 +1,12 @@
 "use strict";
 /* بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ ﷺ InshaAllah */
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.CLOUDINARY_API_SECRET = exports.CLOUDINARY_API_KEY = exports.CLOUDINARY_CLOUD = exports.REDIS_HOST = exports.REDIS_URL = exports.SMTP_PASS = exports.SMTP_USER = exports.MYSQL_DB_PASSWORD = exports.MYSQL_DB_PORT = exports.MYSQL_DB_USERNAME = exports.MYSQL_DB_HOST = exports.MYSQL_DB_URI = exports.MYSQL_DB_DATABASE_NAME = exports.PORT = exports.NODE_ENV = void 0;
+exports.CLOUDINARY_API_SECRET = exports.CLOUDINARY_API_KEY = exports.CLOUDINARY_CLOUD = exports.REDIS_HOST = exports.REDIS_URL = exports.SMTP_PASS = exports.SMTP_USER = exports.MYSQL_DB_PASSWORD = exports.MYSQL_DB_PORT = exports.MYSQL_DB_USERNAME = exports.MYSQL_DB_HOST = exports.MYSQL_DB_URI = exports.MYSQL_DB_DATABASE_NAME = exports.CLIENT_ORIGIN = exports.PORT = exports.NODE_ENV = void 0;
 const dotenv_1 = require("dotenv");
 (0, dotenv_1.config)();
 exports.NODE_ENV = process.env.NODE_ENV;
 exports.PORT = process.env.PORT;
+exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN;
 exports.MYSQL_DB_DATABASE_NAME = process.env.MYSQL_DB_DATABASE_NAME;
 exports.MYSQL_DB_URI = process.env.MYSQL_DB_URI;
 exports.MYSQL_DB_HOST = process.env.MYSQL_DB_HOST;
--- backend/dist/index.js
+++ backend/dist/index.js
@@ -13,7 +13,7 @@ const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)({
-    origin: 'http://localhost:3000',
+    origin: env_1.CLIENT_ORIGIN,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
 }));
